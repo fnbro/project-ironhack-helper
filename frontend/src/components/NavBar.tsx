2668dfbd5c1c462a2c40ebe4d9c4b4d4b81d7b4f
Rookez <54031130+Rookez@users.noreply.github.com>
2019-10-18 11:21:03 +0200

    navbar implementiert

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,14 +1,30 @@
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const nav = ( props: any ) => {
+const nav = (props: any) => {
     return (
         <nav>
-            <ul>
-                <li><NavLink exact={true} to="/">Login</NavLink></li>
-                <li><NavLink to="/register">Register</NavLink></li>
-                <li><NavLink to="/showassets">Assets</NavLink></li>
-            </ul>
+            <div id="ironhack-helper">
+                <NavLink to="/">Ironhack Helper</NavLink>
+            </div>
+            <div id="nav-links">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/news">News</NavLink>
+                <NavLink to="/feedback">Feedback</NavLink>
+                <NavLink to="/forum">Forum</NavLink>
+                <div id="dropdown">
+                    <button id="dropbtn">Extras
+                    <i id="fa fa-caret-down"></i>
+                    </button>
+                    <div id="dropdown-content">
+                        <NavLink to="/random">Pair Programming</NavLink>
+                        <NavLink to="/links">Helpful Links</NavLink>
+                    </div>
+                </div>
+                <div>
+                    <NavLink to="/login">Login</NavLink>
+                </div>
+            </div>
         </nav>
     )
 }
